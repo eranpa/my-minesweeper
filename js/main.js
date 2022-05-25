93f44af1c049bbb14353353edf5ebf2c5b030f7c
eranpa <eranpascal@gmail.com>
2022-05-25 20:29:42 +0300

    first submission

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var gBoard
-var gLevel = {size: 4, mines: 2}
+var gLevel = { size: 4, mines: 2 }
 var gGame
 
 var FLAG = '🚩'
@@ -13,7 +13,8 @@ function initGame() {
     console.log('hey')
     gBoard = buildBoard()
     console.table(gBoard)
-
+    // printMat(gBoard, )
+    renderBoard(gBoard)
 }
 // TODO: buildBoard  - builds the board model - set mines at random locations
 // call setMinesNegsCount, return the created board
@@ -23,6 +24,7 @@ function buildBoard() {
     for (var i = 0; i < gLevel.size; i++) {
         board[i] = [];
         for (var j = 0; j < gLevel.size; j++) {
+
             var cell = {
                 minesAroundCount: 0,
                 isShown: false,
@@ -34,6 +36,13 @@ function buildBoard() {
         }
     }
 
+    //locate mines randomly
+    locateMines(board)
+
+
+    //count mines around each cell and update minesAroundCount property for each cell 
+    setMinesNegsCount(board)
+
     return board;
 }
 
@@ -44,25 +53,74 @@ function buildBoard() {
 //minesAroundCount.
 
 function setMinesNegsCount(board) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[0].length; j++) {
 
+            for (var k = i - 1; k <= i + 1; k++) {
+                if (k < 0 || k >= board.length) continue;
+                for (var l = j - 1; l <= j + 1; l++) {
+                    if (k === i && l === j) continue;
+                    if (l < 0 || l >= board[k].length) continue;
+                    if (board[k][l].isMine) board[i][j].minesAroundCount++
+                }
+            }
+        }
+
+    }
 }
 
 // TODO:  renderBoard(board) - Render the board as a <table> to the page
 
 function renderBoard(board) {
+    var strHTML = '<table border="0"><tbody>';
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>';
+        for (var j = 0; j < board[0].length; j++) {
 
+
+
+
+            var classId = 'cell-' + i + '-' + j;
+            strHTML += `<td id= "classId" class="cell" onclick= "cellClicked(this, ${i}, ${j})" oncontextmenu = "cellMarked(this, ${i}, ${j})"> </td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>';
+    var elContainer = document.querySelector('.board-container');
+    elContainer.innerHTML = strHTML;
 }
+
 // TODO: cellClicked(elCell, i, j) - Called when a cell (td) is clicked
 
 function cellClicked(elCell, i, j) {
 
+    console.log("clicked") //TODO: GET RID 
+    if (gBoard[i][j].isMine) {
+        gameLost()
+        return
+    } else if (gBoard[i][j].minesAroundCount === 0) {
+        elCell.classList.add('revealed')
+        expandShown(gBoard, elCell, i, j)
+    } else {
+        console.log('display') //TODO: GET RID 
+        elCell.classList.add('revealed')
+        var displayInCell = `${gBoard[i][j].minesAroundCount}`
+        elCell.innerHTML = displayInCell
+    }
+
+    gBoard[i][j].isShown = true
 }
 
 // TODO: cellMarked(elCell) -Called on right click to mark a cell (suspected to be a mine)
 //Search the web (and implement) how to hide the context menu on right click
 
 
-function cellMarked(elCell) {
+function cellMarked(elCell, i, j) {
+    console.log('right clicked') //TODO: GET RID 
+    var cell = gBoard[i][j]
+    cell.isMarked = cell.isMarked !== true
+    var cellDisp = cell.isMarked ? FLAG : ' '
+    elCell.innerHTML = cellDisp
 
 }
 
@@ -75,11 +133,25 @@ function checkGameOver() {
 //mines around, we need to open not only that cell, but also its neighbors 
 
 function expandShown(board, elCell, i, j) {
+    console.log('expand')
+    for (var k = i - 1; k <= i + 1; k++) {
+        if (k < 0 || k >= board.length) continue;
+        for (var l = j - 1; l <= j + 1; l++) {
+            if (k === i && l === j) continue;
+            if (l < 0 || l >= board[k].length) continue;
+            var elNeig = 
+        }
+    }
+}
 
+//TODO: game lost -  reveal the whole board, stop timer, disable game, change emoji
+function gameLost() {
+    console.log('game lost')
 }
 
 ///////////setLevel/////////
 // TODO: connect to html 
+//called on onclick assigned to each option on the display 
 function setLevel(userLevelChoice) {
     switch (userLevelChoice) {
         case 'Beginner':
@@ -100,5 +172,16 @@ function setLevel(userLevelChoice) {
                 mines: 30
             }
             break
+    }
+}
+
+
+//////locateMines///////
+//locate mines randomly
+
+function locateMines(board) {
+    for (var i = 0; i < gLevel.mines; i++) {
+        var newMineLocation = getEmptyCell(board)
+        board[newMineLocation.i][newMineLocation.j].isMine = true
     }
 }
---
+++ js/utils.js
@@ -0,0 +1,23 @@
+function getEmptyCell(board) {
+    var emptyCells = []
+    for (var i = 0; i < board.length; i++) {
+      for (var j = 0; j < board[0].length; j++) {
+        var cell = board[i][j]
+        if (!cell.isMine) {
+          emptyCells.push({ i: i, j: j })
+        }
+      }
+    }
+  
+    if (emptyCells.length === 0) return
+    var randIdx = getRandomInt(0, emptyCells.length)
+    return emptyCells[randIdx]
+  }
+  
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+
